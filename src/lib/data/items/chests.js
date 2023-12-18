308e59bb5edabc099056eeee68c0a18786025467
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2023-12-18 14:42:27 -0800

    Add chests data.

---
+++ src/lib/data/items/chests.js
@@ -0,0 +1,374 @@
+export default [
+  {
+    location: 'Bandit Cave',
+    chests: [
+      { name: 'Spark' },
+      { name: 'White Ash' },
+      { name: 'Brown Herb' },
+    ]
+  },
+  {
+    location: 'Harlinton Forest',
+    chests: [
+      { name: 'Silver Dust' },
+      { name: 'Copper Ring' },
+      { name: 'Ice Cube' },
+    ]
+  },
+  {
+    location: 'Blackwater Port',
+    chests: [
+      { name: 'Battery' },
+    ]
+  },
+  {
+    location: 'Mayfall City',
+    tip: 'Chests #2-4 only accessible after the first Battle at Mayfall City (Chapter 4)',
+    chests: [
+      { name: 'Stone Amulet' },
+      { name: 'Battery' },
+      { name: 'Vigor' },
+      { name: 'Feather Robe' },
+    ]
+  },
+  {
+    location: 'Mayfall Forest',
+    chests: [
+      { name: 'Feathered Hat' },
+      { name: 'Black Ash' },
+      { name: 'Mend' },
+    ]
+  },
+  {
+    location: "Amen Corner: Deke's Mansion",
+    tip: "Only accessible during Joe's recruitment quest",
+    chests: [
+      { name: 'Neutralize' },
+    ]
+  },
+  {
+    location: 'Western Pass',
+    chests: [
+      { name: 'Shadowcast' },
+      { name: 'Hide Belt' },
+      { name: 'Full Helmet' },
+    ]
+  },
+  {
+    location: 'Treasure Island',
+    chests: [
+      { name: 'Bronze Plate' },
+      { name: 'Ice Cube' },
+      { name: 'Silver Dust' },
+      { name: 'Clearsight' },
+      { name: '500 Arn' },
+    ]
+  },
+  {
+    location: "Midian's Island",
+    chests: [
+      { name: 'Pointed Hat' },
+    ]
+  },
+  {
+    location: 'Credence Castle',
+    tip: 'Chests only accessible during initial visit (Chapter 3)',
+    chests: [
+      { name: 'Studded Leather' },
+      { name: 'Stone Amulet' },
+      { name: 'Cotton Scarf' },
+      { name: 'Lightray' },
+      { name: 'White Ash' },
+      { name: 'Vigor' },
+    ]
+  },
+  {
+    location: 'Ansee',
+    chests: [
+      { name: 'Splash' },
+    ]
+  },
+  {
+    location: 'Grunthall: Highland Mines',
+    chests: [
+      { name: 'Noise' },
+      { name: 'Mend-All' },
+      { name: 'Steel Bracer' },
+      { name: 'Red Cinder' },
+      { name: 'Light Helm' },
+      { name: 'Icicle' },
+    ]
+  },
+  {
+    location: 'Grunthall: Mt. Bosch',
+    chests: [
+      { name: 'Bronze Ring' },
+    ]
+  },
+  {
+    location: 'Mayfall Forest: Mist Maze',
+    chests: [
+      { name: 'Gold Dust' },
+      { name: 'Green Herb' },
+      { name: 'Cotton Scarf' },
+      { name: 'Silk Beret' },
+      { name: 'Mend-All' },
+      { name: 'Quartz Amulet' },
+      { name: 'Frost Shard' },
+      { name: 'Steel Boots' },
+    ]
+  },
+  {
+    location: "Myst's House",
+    tip: 'Chest only appears from Chapter 5 onward',
+    chests: [
+      { name: 'Charged Jet' },
+    ]
+  },
+  {
+    location: 'Oischin Forest',
+    chests: [
+      { name: 'Lightning Rod' },
+      { name: 'Charged Jet' },
+      { name: 'Green Herb' },
+      { name: 'Restore' },
+      { name: 'Green Glasses' },
+    ]
+  },
+  {
+    location: 'Elemental Mansion',
+    chests: [
+      { name: 'Green Herb' },
+      { name: 'Velvet Scarf' },
+      { name: 'Quartz Amulet' },
+      { name: 'Menu Set #5' },
+      { name: 'Enlight' },
+      { name: 'Mend-All' },
+      { name: 'Swift Shoes' },
+      { name: 'Purple Herb' },
+    ]
+  },
+  {
+    location: 'Fading Dream',
+    chests: [
+      { name: 'Flame Ball' },
+      { name: 'Long Boots' },
+      { name: 'Heavy Vest' },
+      { name: 'Clearmind' },
+      { name: 'Restore' },
+      { name: 'Darkstrike' },
+      { name: 'Frost Shard' },
+      { name: 'Gold Dust' },
+    ]
+  },
+  {
+    location: 'Matrech State Capital: Courthouse',
+    tip: 'Only accessible during the initial visit (Chapter 5)',
+    chests: [
+      { name: 'Fireball' },
+      { name: 'Star Gown' },
+    ]
+  },
+  {
+    location: "Gaia's Heart",
+    chests: [
+      { name: 'Gold Dust' },
+      { name: 'Icestrike' },
+      { name: 'Star Gown' },
+      { name: 'Red Bandana' },
+      { name: 'Red Cape' },
+      { name: 'Red Glasses' },
+    ]
+  },
+  {
+    location: 'Lost City of Avalon',
+    chests: [
+      { name: 'Templar Helmet' },
+      { name: 'Waterfall' },
+      { name: 'Gold Dust' },
+      { name: 'Gold Dust' },
+      { name: 'Templar Greaves' },
+      { name: 'Templar Plate' },
+      { name: 'Opal Amulet' },
+    ]
+  },
+  {
+    location: 'Bergstadt',
+    tip: 'Chest only accessible post-liberation (Chapter 6)',
+    chests: [
+      { name: 'Battle Plan #2' },
+    ]
+  },
+  {
+    location: 'Kelsinger Pass',
+    chests: [
+      { name: 'Restore' },
+      { name: 'Firestrike' },
+      { name: 'Charged Pearl' },
+      { name: 'Charged Jet' },
+      { name: 'Menu Set #4' },
+      { name: 'Light Turban' },
+    ]
+  },
+  {
+    location: 'Matrech Catacombs',
+    chests: [
+      { name: 'Warm Parka' },
+      { name: 'Restore' },
+      { name: 'Velvet Scarf' },
+      { name: 'Lightning Rod' },
+      { name: 'Gold Dust' },
+      { name: 'Gold Dust' },
+      { name: 'Marid Tear' },
+    ]
+  },
+  {
+    location: 'Matrech State Capital',
+    tip: 'Chest only accessible after clearing the Matrech Catacombs (Chapter 7), with Ryan in your party',
+    chests: [
+      { name: 'Battle Plan #7' },
+    ]
+  },
+  {
+    location: 'Vishnu Citadel',
+    chests: [
+      { name: 'Chain Belt' },
+      { name: 'Balance' },
+      { name: 'Fireshield' },
+      { name: 'Rainbow Ore' },
+      { name: 'Knight Armor' },
+      { name: 'Cure' },
+    ]
+  },
+  {
+    location: 'Magma Caverns',
+    chests: [
+      { name: 'Menu Set #7' },
+      { name: 'Satin Scarf' },
+      { name: 'Firestrike' },
+    ]
+  },
+  {
+    location: 'Shiva Passage',
+    tip: "H = Harvey's team, D = Daniel's team, J = Jovian's team, A = Ayara's team",
+    chests: [
+      { no: 'H1.', name: 'Titanium Bracer' },
+      { no: 'D1.', name: 'Opal Amulet' },
+      { no: 'D2.', name: 'Fine Silk Robe' },
+      { no: 'D3.', name: 'Gold Dust' },
+      { no: 'J1.', name: 'Cure' },
+      { no: 'J2.', name: 'Iceshield' },
+      { no: 'J3.', name: 'Flame Ball' },
+      { no: 'J4.', name: 'Titanium Orb' },
+      { no: 'A1.', name: 'Light Turban' },
+    ]
+  },
+  {
+    location: 'Levier',
+    chests: [
+      { name: 'Battle Plan #5' },
+    ]
+  },
+  {
+    location: 'Forgotten Cave',
+    chests: [
+      { name: 'Cure-All' },
+      { name: 'Golden Hat' },
+    ]
+  },
+  {
+    location: 'Jinen Gama',
+    chests: [
+      { name: 'Studded Belt' },
+      { name: 'Charged Jet' },
+      { name: 'Eclipse' },
+    ]
+  },
+  {
+    location: 'Ft. Brahma',
+    chests: [
+      { name: 'Battle Plan #8' },
+    ]
+  },
+  {
+    location: 'Mt. Ea',
+    chests: [
+      { name: 'Battle Plan #9' },
+      { name: 'Thundershield' },
+    ]
+  },
+  {
+    location: 'Jormundgand',
+    chests: [
+      { name: 'Golden Vest' },
+      { name: 'Cure-All' },
+      { name: 'Lightning Rod' },
+      { name: 'Rainbow Ore' },
+    ]
+  },
+  {
+    location: 'Hall of Memories',
+    chests: [
+      { name: 'Purple Glasses' },
+      { name: 'Crystal Battlehelm' },
+      { name: 'Battle Plan #11' },
+      { name: 'Goddess Robe' },
+      { name: 'Blink' },
+    ]
+  },
+  {
+    location: 'Nocturne_Keep',
+    chests: [
+      { name: 'Diamond Dust' },
+      { name: 'Shield' },
+      { name: 'Nirvana Hat' },
+      { name: 'Giant Belt' },
+      { name: 'Purple Herb' },
+    ]
+  },
+  {
+    location: 'Spiritual Plane',
+    chests: [
+      { name: 'Aquabeam' },
+      { name: 'Sagely Robe' },
+      { name: 'Battle Plan #12' },
+    ]
+  },
+  {
+    location: 'Shadow_Clan',
+    chests: [
+      { name: 'Ninja Gear' },
+      { name: 'Shockfield' },
+      { name: 'Heal-All' },
+    ]
+  },
+  {
+    location: 'Jotunheim',
+    chests: [
+      { name: 'Battle Plan #13' },
+    ]
+  },
+  {
+    location: 'Vanaheim',
+    chests: [
+      { name: 'Purple Bandana' },
+      { name: 'Purple Herb' },
+      { name: 'Heal-All' },
+      { name: 'Black Ruby' },
+      { name: 'Regen' },
+      { name: 'Menu Set #10' },
+      { name: 'Battle Plan #21' },
+      { name: 'Imperial Boots' },
+      { name: 'Topaz Amulet' },
+      { name: 'Haste' },
+      { name: 'Rainbow Ore' },
+    ]
+  },
+  {
+    location: 'Elysium Castle',
+    tip: 'Chest can only be opened after recruiting all 75 playable characters',
+    chests: [
+      { name: 'Battle Plan #23' },
+    ]
+  }
+]
