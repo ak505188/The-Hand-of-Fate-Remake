535339a9d42e00fde1116277b92c409440d70d4e
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2023-12-24 01:59:05 -0800

    Element component attempt, untested

---
+++ src/lib/components/Element.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Elements from '$lib/data/elements.js'
+  export let element;
+</script>
+
+{#if element == Elements.Light}
+  <span style="color:var(--element-light)">&#9670;</span>
+{:else if element == Elements.Dark}
+  <span style="color:var(--element-dark)">&#9632;</span>
+{:else if element == Elements.Fire}
+  <span style="color:var(--element-fire)">&#9650;</span>
+{:else if element == Elements.Ice}
+  <span style="color:var(--element-ice)">&#9660;</span>
+{:else if element == Elements.Water}
+  <span style="color:var(--element-water)">&#9679;</span>
+{:else if element == Elements.Lightning}
+  <span style="color:var(--element-lightning)">&#9733;</span>
+{:else}
+  <span/>
+{/if}
---
+++ src/lib/data/elements.js
@@ -0,0 +1,8 @@
+export default {
+  Light: 'Light',
+  Dark: 'Dark',
+  Fire: 'Fire',
+  Ice: 'Ice',
+  Water: 'Water',
+  Lightning: 'Lightning'
+}
