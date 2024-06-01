f46a55cb2c82bd480d16bfaa7a646f0ba54bf4b7
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2024-06-01 14:52:46 -0700

    Generated encounters.js

---
+++ src/lib/data/encounters.js
@@ -0,0 +1,445 @@
+export const Encounters = [
+  {
+    chapter: 1,
+    location: "Bandit Cave",
+    encounters: [
+      { arn: 1, encounter: "1x Bat" },
+      { arn: 3, encounter: "2x Bat" },
+      { arn: 5, encounter: "3x Bandit (Axe)" },
+      { arn: 7, encounter: "3x Bandit (Axe) + 1x Bandit (bow)" },
+      { arn: 8, encounter: "2x Bandit (Axe) + 2x Bandit (bow)" },
+    ]
+  },
+  {
+    chapter: 2,
+    location: "World Map - Helman Island",
+    encounters: [
+      { arn: 3, encounter: "Spider" },
+      { arn: 4, encounter: "2x Puddle" },
+      { arn: 5, encounter: "3x Killer Hornet" },
+      { arn: 5, encounter: "Puddle + 2x Killer Hornet" },
+      { arn: 6, encounter: "2x Spider" },
+      { arn: 7, encounter: "4x Killer Hornet" },
+    ]
+  },
+  {
+    chapter: 2,
+    location: "Harlinton Forest",
+    note: "Enemies only available on first story visit",
+    encounters: [
+      { arn: 4, encounter: "2x Kirgard 4th" },
+      { arn: 7, encounter: "2x Kirgard 4th + 1x Kirgard Sergeant" },
+    ]
+  },
+  {
+    chapter: 2,
+    location: "World Map - Mayfall",
+    encounters: [
+      { arn: 7, encounter: "2x Grey Wolf" },
+      { arn: 7, encounter: "2x Goblin" },
+      { arn: 8, encounter: "4x Green Viper" },
+      { arn: 9, encounter: "1x Goblin + 3x Green Viper" },
+      { arn: 10, encounter: "3x Grey Wolf" },
+    ]
+  },
+  {
+    chapter: 2,
+    location: "Mayfall Forest",
+    encounters: [
+      { arn: 10, encounter: "3x Goblin" },
+      { arn: 10, encounter: "3x Grey Wolf" },
+      { arn: 13, encounter: "2x Lizardmen + 2x Goblin" },
+      { arn: 13, encounter: "2x Lizardmen + 2x Grey Wolf" },
+      { arn: 14, encounter: "2x Goblin + 1x Orc" },
+    ]
+  },
+  {
+    chapter: 2,
+    location: "World Map - Matrech",
+    encounters: [
+      { arn: 12, encounter: "1x Wandering Mage" },
+      { arn: 14, encounter: "2x Orc" },
+      { arn: 15, encounter: "3x Grey Wolf + 1x Matrian Wolf" },
+    ]
+  },
+  {
+    chapter: 3,
+    location: "Deke’s Mansion (optional)",
+    encounters: [
+      { arn: 11, encounter: "2x Wood Soldier" },
+      { arn: 14, encounter: "2x Surprise Box" },
+      { arn: 18, encounter: "1x Surprise Box + 2x Wood Soldier" },
+    ]
+  },
+  {
+    chapter: 3,
+    location: "Western Pass",
+    encounters: [
+      { arn: 14, encounter: "3x Matrian Wolf" },
+      { arn: 15, encounter: "2x Hill Orc" },
+      { arn: 21, encounter: "3x Mud Golem + 1x Hermit" },
+      { arn: 22, encounter: "1x Orc + 2x Hill Orc" },
+    ]
+  },
+  {
+    chapter: 3,
+    location: "Treasure Island",
+    encounters: [
+      { arn: 16, encounter: "2x Yellow Puddle" },
+      { arn: 23, encounter: "1x Maneater" },
+      { arn: 24, encounter: "4x Mantis" },
+      { arn: 28, encounter: "2x Mantis + 1x Forest Spirit" },
+      { arn: 32, encounter: "2x Yellow Puddle + 1x Forest Spirit" },
+    ]
+  },
+  {
+    chapter: 3,
+    location: "Credence Castle",
+    encounters: [
+      { arn: 25, encounter: "2x Zombie" },
+      { arn: 29, encounter: "2x Dead Knight + 1x Zombie" },
+      { arn: 30, encounter: "4x Vampire Bat" },
+      { arn: 47, encounter: "3x Vampire Bat + 1x Spectral Mage" },
+      { arn: 49, encounter: "3x Dead Knight + 1x Spectral Mage" },
+    ]
+  },
+  {
+    chapter: 4,
+    location: "World Map - Highland",
+    encounters: [
+      { arn: 31, encounter: "4x Hill Orc" },
+      { arn: 35, encounter: "2x Minotaur" },
+      { arn: 38, encounter: "1x Ogre" },
+      { arn: 40, encounter: "3x Hill Orc + 1x Minotaur" },
+    ]
+  },
+  {
+    chapter: 4,
+    location: "Highland Mines",
+    encounters: [
+      { arn: 53, encounter: "1x Guard Tiger + 2x Highlander" },
+      { arn: 56, encounter: "5x Highlander " },
+      { arn: 56, encounter: "1x Guard Tiger + 1x Highlander + 1x Highland Scout" },
+      { arn: 61, encounter: "3x Highlander + 2x Highland Scout " },
+      { arn: 67, encounter: "6x Highlander " },
+    ]
+  },
+  {
+    chapter: 4,
+    location: "Mt. Bosch",
+    encounters: [
+      { arn: 27, encounter: "2x Wild Barbarian" },
+      { arn: 52, encounter: "3x Minotaur" },
+      { arn: 70, encounter: "1x Ogre + 1x Minotaur + 1x Wild Barbarian" },
+      { arn: 73, encounter: "2x Minotaur + 1x Ogre" },
+    ]
+  },
+  {
+    chapter: 4,
+    location: "Mist Maze (optional)",
+    encounters: [
+      { arn: 58, encounter: "1x Mist Monster" },
+      { arn: 90, encounter: "3x Proto-Mist" },
+      { arn: 117, encounter: "2x Mist Monster (Only on the deep fog area)" },
+      { arn: 118, encounter: "2x Proto-Mist + 1x Mist Monster (Only on the deep fog area)" },
+    ]
+  },
+  {
+    chapter: 4,
+    location: "Mayfall City",
+    note: "enemies only available on the Mayfall battle story event",
+    encounters: [
+      { arn: 63, encounter: "6x Kirgard 2nd" },
+      { arn: 88, encounter: "5x Kirgard 2nd + 1x Kirgard Sergeant" },
+    ]
+  },
+  {
+    chapter: 5,
+    location: "Oischin Forest",
+    encounters: [
+      { arn: 80, encounter: "4x Black Cobra" },
+      { arn: 92, encounter: "2x Wood Berserker" },
+      { arn: 107, encounter: "2x Dragon Man + 2x Black Cobra" },
+      { arn: 141, encounter: "1x Basilisk" },
+      { arn: 147, encounter: "3x Dragon Man + 1x Wood Berserker" },
+    ]
+  },
+  {
+    chapter: 5,
+    location: "World Map - Oischin",
+    encounters: [
+      { arn: 92, encounter: "2x Wood Berserker" },
+      { arn: 107, encounter: "2x Devourer" },
+      { arn: 141, encounter: "1x Basilisk" },
+      { arn: 147, encounter: "3x Dragon Man + 1x Wood Berserker" },
+    ]
+  },
+  {
+    chapter: 5,
+    location: "Elemental Mansion",
+    encounters: [
+      { arn: 157, encounter: "1x Blue Elemental + 1x White Elemental + 1x Black Elemental" },
+      { arn: 161, encounter: "3x Black Elemental" },
+      { arn: 205, encounter: "4x Blue Elemental" },
+      { arn: 210, encounter: "4x White Elemental" },
+    ]
+  },
+  {
+    chapter: 5,
+    location: "Fading Dream",
+    encounters: [
+      { arn: 187, encounter: "2x Brain Leech + 1x Lurker" },
+      { arn: 210, encounter: "2x Lurker" },
+      { arn: 240, encounter: "3x Light Stalker" },
+      { arn: 242, encounter: "2x Light Stalker + 2x Brain Leech" },
+      { arn: 312, encounter: "1x Cathead" },
+    ]
+  },
+  {
+    chapter: 6,
+    location: "Gaia’s Heart",
+    encounters: [
+      { arn: 250, encounter: "2x Belgugon + 1x Carrion Worm" },
+      { arn: 307, encounter: "3x Carrion Worm" },
+      { arn: 327, encounter: "2x Carrion Worm + 2x Dire Spider " },
+      { arn: 331, encounter: "2x Belgugon + 3x Dire Spider " },
+      { arn: 418, encounter: "1x Cave Troll" },
+    ]
+  },
+  {
+    chapter: 6,
+    location: "Lost City of Avalon (optional)",
+    encounters: [
+      { arn: 446, encounter: "3x Enchanter" },
+      { arn: 538, encounter: "1x Templar + 3x Crusader" },
+      { arn: 540, encounter: "2x Enchanter + 2x Crusader" },
+      { arn: 606, encounter: "5x Crusader" },
+      { arn: 617, encounter: "1x Unicorn + 2x Crusader" },
+      { arn: 766, encounter: "1x Enchanter + 1x Unicorn + 2x Crusader" },
+    ]
+  },
+  {
+    chapter: 6,
+    location: "Kelsinger Pass",
+    encounters: [
+      { arn: 540, encounter: "1x Orc Shaman + 3x Ogre Soldier" },
+      { arn: 550, encounter: "1x Nightmare + 2x Ogre Soldier" },
+      { arn: 600, encounter: "2x Nightmare" },
+      { arn: 625, encounter: "5x Ogre Soldier" },
+      { arn: 625, encounter: "1x Hill Giant" },
+      { arn: 747, encounter: "1x Orc Shaman + 1x Ogre Chieftain + 3x Ogre Soldier" },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "Matrech Catacombs",
+    note: "Enemies only available on first story visit",
+    encounters: [
+      { arn: 682, encounter: "1x Matrech Elite + 3x Matrech Knight **" },
+      { arn: 700, encounter: "5x Matrech Knight **" },
+      { arn: 822, encounter: "1x Matrech Elite + 4x Matrech Knight **" },
+      { arn: 825, encounter: "3x Sahagin" },
+      { arn: 937, encounter: "1x Land Shark + 2x Sahagin " },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "World Map - South Kirgard",
+    encounters: [
+      { arn: 650, encounter: "1x Hoarder + 3x Azure Golem" },
+      { arn: 775, encounter: "2x Land Shark" },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "Vishnu Citadel",
+    encounters: [
+      { arn: 900, encounter: "4x Tribal Warrior" },
+      { arn: 975, encounter: "2x Tribal Warrior + 2x Hunter " },
+      { arn: 1000, encounter: "2x Savak" },
+      { arn: 1000, encounter: "1x Warlock + 3x Tribal Warrior" },
+      { arn: 1087, encounter: "1x Warlock + 1x Hunter + 1x Savak" },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "Magma Caverns (optional)",
+    encounters: [
+      { arn: 1087, encounter: "3x Hobgoblin" },
+      { arn: 1125, encounter: "2x Gargoyle" },
+      { arn: 1150, encounter: "1x Imp + 2x Hobgoblin" },
+      { arn: 1215, encounter: "4x Lava Bones" },
+      { arn: 1406, encounter: "1x Catoblepas" },
+      { arn: 1532, encounter: "1x Gargoyle + 1x Hobgoblin + 2x Lava Bones" },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "Shiva Passage",
+    note: "Enemies only available on first story visit",
+    encounters: [
+      { arn: 1300, encounter: "4x Almenga Soldier" },
+      { arn: 1500, encounter: "2x Parasite + 1x Mountain Tiger" },
+      { arn: 1512, encounter: "3x Almenga Soldier + 1x Almenga Captain" },
+      { arn: 1600, encounter: "1x Almenga Summoner + 1x Pandora's Box" },
+      { arn: 1600, encounter: "1x Almenga Summoner + 2x Pandora's Box" },
+      { arn: 1625, encounter: "5x Almenga Soldier" },
+      { arn: 1687, encounter: "1x Manticore" },
+      { arn: 1837, encounter: "4x Almenga Soldier + 1x Almenga Captain" },
+      { arn: 1850, encounter: "4x Parasite" },
+      { arn: 2262, encounter: "1x Manticore + 1x Mountain Tiger" },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "World Map - North Kirgard",
+    encounters: [
+      { arn: 1, encounter: "2x Pandora’s Box" },
+      { arn: 1000, encounter: "2x Green Wolf + 1x Pandora’s Box" },
+      { arn: 1150, encounter: "2x Mountain Tiger" },
+      { arn: 2000, encounter: "4x Green Wolf" },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "Jinen Gama",
+    encounters: [
+      { arn: 2075, encounter: "1x Sand Shark" },
+      { arn: 2600, encounter: "4x Black Death" },
+      { arn: 2625, encounter: "3x Harpy" },
+      { arn: 2800, encounter: "1x Brown Serpent + 3x Black Death" },
+      { arn: 3450, encounter: "2x Harpy + 2x Brown Serpent" },
+    ]
+  },
+  {
+    chapter: 7,
+    location: "Mt Ea (optional)",
+    encounters: [
+      { arn: 2475, encounter: "1x Paragon + 1x Deathgaze" },
+      { arn: 2700, encounter: "1x Deathgaze + 2x Earth Worm" },
+      { arn: 3000, encounter: "1x Chimaera" },
+      { arn: 3487, encounter: "2x Paragon + 3x Earth Worm" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Jormundgand",
+    encounters: [
+      { arn: 2718, encounter: "5x Flying Fish" },
+      { arn: 3181, encounter: "2x Drow Fairy + 3x Flying Fish" },
+      { arn: 3237, encounter: "2x Needler Tortoise + 2x Flying Fish" },
+      { arn: 3356, encounter: "1x Flying Fish + 1x Frost Giant" },
+      { arn: 3887, encounter: "1x Needler Tortoise + 1x Frost Giant" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "World Map - Mirgard Plains",
+    encounters: [
+      { arn: 1750, encounter: "2x Snow Worm" },
+      { arn: 2000, encounter: "1x Snow Worm + 2x Ice Wolf" },
+      { arn: 2250, encounter: "4x Ice Wolf" },
+      { arn: 2718, encounter: "5x Flying Fish" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Nocturn Keep (optional)",
+    encounters: [
+      { arn: 5000, encounter: "2x Little Scarlet" },
+      { arn: 5362, encounter: "1x Harbinger + 1x Wraith + 2x Ghoul" },
+      { arn: 5500, encounter: "2x Wraith + 1x Little Scarlet" },
+      { arn: 6700, encounter: "2x Harbinger + 3x Ghoul" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Hall of Memories",
+    encounters: [
+      { arn: 5250, encounter: "2x Deep Spawn" },
+      { arn: 5400, encounter: "3x Merman" },
+      { arn: 5875, encounter: "2x Stalker + 1x Deep Spawn" },
+      { arn: 6000, encounter: "1x Black Cat " },
+      { arn: 6850, encounter: "2x Stalker + 2x Merman" },
+      { arn: 7250, encounter: "1x Charybdis + 1x Scrylla " },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Shadow Clan (optional)",
+    encounters: [
+      { arn: 7000, encounter: "5x Shadow Clan" },
+      { arn: 7400, encounter: "1x Shadow Assassin + 3x Shadow Clan" },
+      { arn: 8100, encounter: "2x Navar" },
+      { arn: 8250, encounter: "1x Navar + 3x Shadow Clan" },
+      { arn: 8400, encounter: "6x Shadow Clan" },
+      { arn: 9200, encounter: "2x Shadow Assassin + 2x Shadow Clan" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Spiritual Plane",
+    encounters: [
+      { arn: 8000, encounter: "4x Non-Euclidean Horror" },
+      { arn: 8268, encounter: "3x Eyestalker" },
+      { arn: 9000, encounter: "1x Mara" },
+      { arn: 9381, encounter: "1x Poltergeist + 1x Eyestalker + 2x Non-Euclidean Horror" },
+      { arn: 10762, encounter: "2x Poltergiest + 2x Eyestalker" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "World Map - North Almenga",
+    encounters: [
+      { arn: 9500, encounter: "2x Mustang" },
+      { arn: 10000, encounter: "1x Thunderbeast" },
+      { arn: 10575, encounter: "2x Snow Tortoise" },
+      { arn: 15325, encounter: "2x Snow Tortoise + 1x Mustang" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Vaneheim 1F",
+    encounters: [
+      { arn: 8725, encounter: "1x Almenga Colonel + 3x Almenga Elite " },
+      { arn: 9750, encounter: "5x Almenga Elite" },
+      { arn: 10675, encounter: "1x Almenga Colonel + 4x Almenga Elite" },
+      { arn: 11500, encounter: "1x Grand Conjurer + 2x Lightning Bolt" },
+      { arn: 12250, encounter: "1x Grand Conjurer + 3x Lightning Bolt" },
+      { arn: 12500, encounter: "1x Dracoknight" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Vaneheim 2F",
+    encounters: [
+      { arn: 12500, encounter: "1x Dracoknight" },
+      { arn: 12937, encounter: "2x Almenga Elite (bow) + 3x Almenga Elite (axe)" },
+      { arn: 14325, encounter: "1x Almega General + 1x Almenga Elite (bow) + 3x Almenga Elite (axe)" },
+      { arn: 14737, encounter: "2x Almenga Elite (bow) + 2x Almenga Elite (axe) + 1x Almenga General" },
+      { arn: 15750, encounter: "1x Apocalypse" },
+      { arn: 17225, encounter: "1x Dracoknight + 2x Almenga Elite (bow)" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Vaneheim 3F",
+    encounters: [
+      { arn: 13125, encounter: "1x Red Dracoknight" },
+      { arn: 13200, encounter: "4x Almenga Elite (bow) + 1x Almenga General" },
+      { arn: 16500, encounter: "2x Starshine + 1x Sirius Lux" },
+      { arn: 17850, encounter: "1x Red Dracoknight + 2x Almenga Elite (bow)" },
+      { arn: 21862, encounter: "1x Almenga Elite (bow) + 1x Almenga General + 1x Apocalypse" },
+      { arn: 31500, encounter: "2x Apocalypse" },
+    ]
+  },
+  {
+    chapter: 8,
+    location: "Shadow Area (optional)",
+    encounters: [
+      { arn: 50000, encounter: "4x Shadow Legionaire" },
+      { arn: 62500, encounter: "5x Shadow Legionaire" },
+      { arn: 75000, encounter: "6x Shadow Legionaire" },
+    ]
+  }
+]
