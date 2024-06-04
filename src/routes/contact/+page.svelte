385a941e0be5b0a859be9b047eb6d7a9e2e96836
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2024-06-04 13:43:57 -0700

    Add contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,7 +4,24 @@
 
 <Container>
   <h2>Contact Information</h2>
+
+  <ul>
+    <li>Website owner: srd_27</li>
+    <li>(Discord contact: @srd_27</li>
+  </ul>
+
+  <ul>
+    <li>Website developer: Z3R01337</li>
+    <li>(Report technical issues <a href="//github.com/ak505188/The-Hand-of-Fate-Remake/issues">here.)</a></li>
+  </ul>
+
+  <p>This website is a remake of the original “The Hand of Fate” guide site made by Chalryn.</p>
+
+  <h3>Disclaimer</h3>
   <p>
-    If there's an issue with the website, you can create an issue on the <a href="//github.com/ak505188/The-Hand-of-Fate-Remake/issues">GitHub</a> here.
+    This website is entirely fan-made, and is not directly affiliated with the creator of the game or the software used to create it. Any contact about this site should be made using the contact details above.
+  </p>
+  <p>
+    Being a fan-made site based on a game, many images used on the site are borrowed from the game. All of said images belong to their rightful owners/creators. We claim no credit for their creation. (This includes all original artwork based on the game, which belongs to SCF.)
   </p>
 </Container>
